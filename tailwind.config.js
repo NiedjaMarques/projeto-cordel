867e69497f4e53f1d2d9bfc85f66cd8fdbedf3eb
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-11-26 14:26:08 -0300

    Implementação do Tailwind CSS no projeto

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        'passion-one':['Passion One', 'sans-serif'],
+        'sriracha':['sriracha', 'sans-serif'],
+        'verdana':['verdana', 'times new roman'],
+      },
+    },
+  },
+  plugins: [],
+}
+
